8975705ac2bbbe8030da0a8955f8efd5b2879187
Danya25 <TDD2001@yandex.ru>
2021-01-03 14:36:59 +0300

    task creator was added

---
+++ MentalstackFront/src/app/main/task-creator/task-creator.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MissionPriority} from '../../models/enums/mission-priority.enum';
+
+@Component({
+  selector: 'app-task-creator',
+  templateUrl: './task-creator.component.html',
+  styleUrls: ['./task-creator.component.css']
+})
+export class TaskCreatorComponent implements OnInit {
+
+  public form: FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      priority: new FormControl(MissionPriority.No, [Validators.required]),
+      endTime: new FormControl('', [Validators.required]),
+      endDate: new FormControl('', [Validators.required])
+    });
+  }
+
+}
