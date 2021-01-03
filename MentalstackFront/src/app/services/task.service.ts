aea8455b632ef18fe24499fcf628b56ecd941574
Danya25 <TDD2001@yandex.ru>
2021-01-03 16:49:42 +0300

    mission services was added

---
+++ MentalstackFront/src/app/services/task.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Mission} from '../models/mission';
+import {Answer} from '../models/answer';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TaskService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    public saveTask(task: Mission): Observable<Answer<boolean>> {
+        return this.http.post<Answer<boolean>>('api/Mission/Save', task);
+    }
+
+    public getAll(): Observable<Answer<Mission[]>> {
+        return this.http.get<Answer<Mission[]>>('api/Mission/GetAll');
+    }
+}
