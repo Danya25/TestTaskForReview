6d13570c6368d63550cd807c940576a9b2c6f508
Danya25 <TDD2001@yandex.ru>
2021-01-04 15:43:44 +0300

    priority select was fixed

--- MentalstackFront/src/app/main/task-creator/task-creator.component.ts
+++ MentalstackFront/src/app/main/task-creator/task-creator.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {MissionPriority} from '../../models/enums/mission-priority.enum';
 
 @Component({
-  selector: 'app-task-creator',
-  templateUrl: './task-creator.component.html',
-  styleUrls: ['./task-creator.component.css']
+    selector: 'app-task-creator',
+    templateUrl: './task-creator.component.html',
+    styleUrls: ['./task-creator.component.css']
 })
 export class TaskCreatorComponent implements OnInit {
+    public form: FormGroup;
 
-  public form: FormGroup;
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      title: new FormControl('', [Validators.required]),
-      priority: new FormControl(MissionPriority.Neutral, [Validators.required]),
-      endTime: new FormControl('', [Validators.required]),
-      endDate: new FormControl('', [Validators.required])
-    });
-  }
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            title: new FormControl('', [Validators.required]),
+            priority: new FormControl(MissionPriority[MissionPriority.Neutral], [Validators.required]),
+            endTime: new FormControl('', [Validators.required]),
+            endDate: new FormControl('', [Validators.required])
+        });
+    }
 
+    public setPriority(priority: number): void {
+        this.form.controls['priority'].setValue(MissionPriority[priority]);
+        console.log(this.form.controls['priority']);
+    }
 }
