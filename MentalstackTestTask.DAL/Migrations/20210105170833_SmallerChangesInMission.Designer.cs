9077f739b455736723c1d69f1ea2eeff5e4c56af
Danya25 <TDD2001@yandex.ru>
2021-01-05 20:24:17 +0300

    migrations

--- MentalstackTestTask.DAL/Migrations/20210105170833_SmallerChangesInMission.Designer.cs
+++ MentalstackTestTask.DAL/Migrations/20210105170833_SmallerChangesInMission.Designer.cs
@@ -4,14 +4,16 @@ using MentalstackTestTask.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MentalstackTestTask.DAL.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210105170833_SmallerChangesInMission")]
+    partial class SmallerChangesInMission
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MentalstackTestTask.DAL/Migrations/20210105170833_SmallerChangesInMission.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MentalstackTestTask.DAL.Migrations
+{
+    public partial class SmallerChangesInMission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Missions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "Missions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Missions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndDate",
+                table: "Missions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
