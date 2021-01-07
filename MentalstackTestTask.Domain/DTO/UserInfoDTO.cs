98d6af11b4c730f4f444953ee08edba3000ee1eb
Danya25 <TDD2001@yandex.ru>
2021-01-07 19:50:32 +0300

    userInfoDTO was added

---
+++ MentalstackTestTask.Domain/DTO/UserInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MentalstackTestTask.Domain.DTO
+{
+    public class UserInfoDTO
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
