2a6c5c51d107f94fd87970ae7b34e2d3b7b8bb98
Daniil Titov <dt@emsoft.ru>
2021-02-08 22:54:27 +0300

    task description info model added

---
+++ MentalstackTestTask.Domain/DTO/TaskDescriptionInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MentalstackTestTask.Domain.DTO
+{
+    public sealed class TaskDescriptionInfoDTO
+    {
+        public string Text { get; set; }
+        public int TaskId { get; set; }
+    }
+}
